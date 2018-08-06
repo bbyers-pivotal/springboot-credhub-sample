725b2c391089eb5ec8e5759e19200444f9f63abe
Brian Byers <brian@xclo.io>
2018-08-06 10:32:53 -0500

    adding lombok annotation to get rid of constructor

--- src/main/java/com/example/credhub/CredhubProperties.java
+++ src/main/java/com/example/credhub/CredhubProperties.java
@@ -2,18 +2,15 @@ package com.example.credhub;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Configuration
 @ConfigurationProperties("vcap.services.mycredhub.credentials") //add the name of your PCF service here to replace "mycredhub"
 public class CredhubProperties {
     public String mysecretkey; //add whatever properties you need in here that are stored in CredHub
-
-    //blank contstructor needed for Lombok
-    public CredhubProperties() {
-
-    }
 }
